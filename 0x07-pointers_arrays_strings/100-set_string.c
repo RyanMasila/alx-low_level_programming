0e3a7187d0b65fb52e8245b3a7688ff46e1c6c99
RyanMasila <ryanmasila12@gmail.com>
2023-06-07 01:53:50 +0300

    Double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * set_string - Entry point
+ * @s: input
+ * @to: input
+ * Return: empty
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H
+#define MAIN_H
+void set_string(char **s, char *to);
+#endif
