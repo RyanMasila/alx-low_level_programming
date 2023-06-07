8b528d96368b6033b222312987323735b5c1c00e
RyanMasila <ryanmasila12@gmail.com>
2023-06-08 01:07:30 +0300

    String length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,9 +5,9 @@
  * @s: string
  * Return: an integer
  */
-int _strlen_recursion (char *s)
+int _strlen_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		return (1 + _strlen_recursion(s + 1));
 	}
