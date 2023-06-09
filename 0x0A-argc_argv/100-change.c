5b024cb28c0302d815c03d074cfc61ca7a165319
RyanMasila <ryanmasila12@gmail.com>
2023-06-10 02:41:53 +0300

    Cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * @argc: Number of arguments
+ * @argv: array of arguments
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int ce, c = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	ce = atoi(argv[1]);
+	while (ce > 0)
+	{
+		c++;
+		if ((ce - 25) >= 0)
+		{
+			ce -= 25;
+			continue;
+		}
+		if ((ce - 10) >= 0)
+		{
+			ce -= 10;
+			continue;
+		}
+		if ((ce - 5) >= 0)
+		{
+			c -= 5;
+			continue;
+		}
+		if ((ce - 2) >= 0)
+		{
+			ce -= 2;
+			continue;
+		}
+		ce--;
+	}
+	printf("%d\n", c);
+	return (0);
+}
