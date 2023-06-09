dde18ff00a6e09245e6a15a5a952d893eb551dfe
RyanMasila <ryanmasila12@gmail.com>
2023-06-09 22:47:00 +0300

    Number of Arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,20 +5,8 @@
  * @argv: array of strings
  * Return: 0
  */
-int main(int argc, char __attribute__ ((unused))*argv[])
+int main(int argc, char __attribute__((unused))*argv[])
 {
-	int i, s = 0;
-
-	if (argc <= 1)
-	{
-		printf("%d\n", s);
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("%d\n", i);
-		}
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
