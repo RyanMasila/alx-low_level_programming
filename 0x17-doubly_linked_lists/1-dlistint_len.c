ee1b7a7cdf12da1412c7aeea3f4564aec57682aa
RyanMasila <ryanmasila12@gmail.com>
2023-08-19 01:59:38 +0300

    . List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - counts no. of elements in lnkd list
+ * @h: head of list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nds = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nds);
+}
