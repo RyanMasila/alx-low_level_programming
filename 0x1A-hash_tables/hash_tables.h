f996037d17034555a0968d60e6bc82dc16193758
RyanMasila <ryanmasila12@gmail.com>
2023-09-09 01:47:19 +0300

    Hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,8 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -29,4 +32,5 @@ typedef struct hash_table_s
      hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 #endif
